d2b24721a83a2d59686497d3a2dd522c61cc49c7
相原 直也 <aiharanaoya@team-lab.com>
2021-12-08 19:06:47 +0900

    add

--- foo/foo.go
+++ foo/foo.go
@@ -1,6 +1,9 @@
 package foo
 
+// 大文字：外部参照可能（変数、関数共通）
 const Max = 100
+
+// 小文字：外部参照不可（変数、関数共通）
 const min = 1
 
 func ReturnMin() int {
--- main.go
+++ main.go
@@ -1,15 +1,25 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	"html/template"
+	"log"
+	"math"
+	"net/http"
+	"os"
+	"time"
 
+	"github.com/aiharanaoya/golang-test/alib"
 	"github.com/aiharanaoya/golang-test/foo"
+
+	_ "github.com/mattn/go-sqlite3"
 )
 
 func main() {
 	fmt.Println("hello world")
 
-	package1()
+	dbTest1()
 }
 
 func test() {
@@ -282,5 +292,337 @@ func interface1() {
 }
 
 func package1() {
-	fmt.Println(foo.Max)
+	fmt.Println(foo.ReturnMin())
 }
+
+// テスト
+func IsOne(i int) bool {
+	if i == 1 {
+		return true
+	} else {
+		return false
+	}
+
+	// return i == 1
+}
+
+func alib1() {
+	s := []int{1, 2, 3, 4, 5}
+	fmt.Println(alib.Average(s))
+}
+
+func study1() {
+	// if文
+	a := 2
+	if a == 1 {
+		fmt.Println("one")
+	} else if a == 2 {
+		fmt.Println("two")
+	} else {
+		fmt.Println("other")
+	}
+
+	// if a := 2; a == 1 {
+	// 	fmt.Println("one")
+	// } else if a == 2 {
+	// 	fmt.Println("two")
+	// } else {
+	// 	fmt.Println("other")
+	// }
+
+	// switch文
+	switch b := 2; b {
+	case 1:
+		fmt.Println("one")
+	case 2:
+		fmt.Println("two")
+	default:
+		fmt.Println("other")
+	}
+
+	switch score := 66; {
+	case score > 80:
+		fmt.Println("Great!")
+	case score > 60:
+		fmt.Println("Good!")
+	default:
+		fmt.Println("Bad")
+	}
+
+	// for文
+	for i := 0; i < 10; i++ {
+		if i == 3 {
+			continue
+		} else if i == 8 {
+			break
+		}
+		fmt.Println(i)
+	}
+
+	c := 0
+	for c < 10 {
+		fmt.Println(c)
+		c++
+	}
+
+	// 配列
+	var ar1 [5]int
+	ar1[1] = 3
+	ar1[2] = 4
+	fmt.Println(ar1)
+
+	ar2 := [5]int{2, 3, 4, 5, 6}
+	fmt.Println(ar2)
+
+	ar3 := [...]int{2, 3, 4, 5, 6}
+	fmt.Println(ar3)
+
+	// スライス
+	ar := [5]int{2, 3, 4, 5, 6}
+
+	sl1 := ar[2:4]
+	fmt.Println(sl1)
+
+	sl2 := ar[2:]
+	fmt.Println(sl2)
+
+	sl3 := ar[:4]
+	fmt.Println(sl3)
+
+	sl4 := ar[:]
+	fmt.Println(sl4)
+
+	sl5 := []int{2, 3, 4, 5, 6}
+	fmt.Println(sl5)
+
+	sl6 := make([]int, 5)
+	fmt.Println(sl6)
+
+	sl7 := append(sl5, 7, 8)
+	fmt.Println(sl7)
+
+	fmt.Println(len(sl7))
+
+	sl8 := sl7
+	fmt.Println(sl8)
+
+	sl8[0] = 100
+	fmt.Println(sl7, sl8)
+
+	sl9 := make([]int, 7)
+	copy(sl9, sl8)
+	fmt.Println(sl9)
+
+	sl9[0] = 200
+	fmt.Println(sl8, sl9)
+
+	// マップ
+	var ma1 = map[string]int{"A": 100, "B": 200}
+	fmt.Println(ma1)
+
+	ma1["A"] = 1000
+	ma1["B"] = 2000
+	fmt.Println(ma1)
+
+	ma2 := map[int]string{
+		1: "A",
+		2: "B",
+		3: "C",
+	}
+	fmt.Println(ma2)
+
+	rangeArr := [3]int{1, 2, 3}
+
+	for i, v := range rangeArr {
+		fmt.Println(i, v)
+	}
+
+	rangeSl := []int{1, 2, 3}
+	for _, v := range rangeSl {
+		fmt.Println(v)
+	}
+
+	// ポインタ
+	var nu1 int = 100
+	fmt.Println(nu1)
+
+	fmt.Println(&nu1)
+
+	var po1 *int = &nu1
+	fmt.Println(po1)
+
+	fmt.Println(*po1)
+
+	double3(nu1)
+	fmt.Println(nu1)
+
+	double4(&nu1)
+	fmt.Println(nu1)
+
+	arPo := [3]int{1, 2, 3}
+	arrayPo(arPo)
+	fmt.Println(arPo)
+
+	slPo := []int{1, 2, 3}
+	slicePo(slPo)
+	fmt.Println(slPo)
+
+	test1()
+}
+
+func double3(i int) {
+	i = i * 2
+}
+
+func double4(i *int) {
+	*i = *i * 2
+}
+
+func arrayPo(i [3]int) {
+	i[0] = 100
+}
+
+func slicePo(i []int) {
+	i[0] = 100
+}
+
+func test1() {
+	sum := 0
+	square := 0
+
+	for i := 1; i <= 100; i++ {
+		sum += (i * i)
+		square += i
+	}
+
+	square = (square * square)
+
+	ans := square - sum
+
+	fmt.Println(ans)
+}
+
+func lib() {
+	f1, err := os.Open("test.txt")
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	defer f1.Close()
+
+	fmt.Println(os.Args[1])
+
+	// f2, _ := os.Create("foo.txt")
+
+	// f2.Write([]byte("Hello\n"))
+	// f2.WriteAt([]byte("Golang"), 6)
+	// f2.Seek(0, os.SEEK_END)
+	// f2.WriteString("AAA\nBBB")
+
+	// 今の時間
+	t1 := time.Now()
+	fmt.Println(t1)
+
+	// 指定時間を生成
+	t2 := time.Date(2021, 11, 29, 20, 0, 0, 0, time.Local)
+	fmt.Println(t2)
+	fmt.Println(t2.Year())
+	fmt.Println(t2.Month())
+	fmt.Println(t2.Weekday())
+	fmt.Println(t2.Day())
+
+	// t1 - t2
+	d1 := t1.Sub(t2)
+	fmt.Println(d1)
+
+	// 円周率
+	fmt.Println(math.Pi)
+
+	// 絶対値
+	fmt.Println(math.Abs(-100))
+
+	// 累乗
+	fmt.Println(math.Pow(2, 3))
+
+	// 平方根
+	fmt.Println(math.Sqrt(2))
+}
+
+// type MyHandler struct{}
+
+// func (h *MyHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+// 	fmt.Fprintf(w, "Hello World")
+// }
+
+// net/http server
+func top(w http.ResponseWriter, r *http.Request) {
+	t, err := template.ParseFiles("tmpl.html")
+	if err != nil {
+		log.Panicln(err)
+	}
+	t.Execute(w, "Hello World")
+}
+
+func server() {
+	http.HandleFunc("/top", top)
+	http.ListenAndServe(":8080", nil)
+}
+
+/////////////////////////////////////
+
+// DB sqlite3
+var Db *sql.DB
+
+func dbTest1() {
+	Db, _ := sql.Open("sqlite3", "./example.sql")
+	defer Db.Close()
+
+	// テーブルの作成
+	// cmd := `CREATE TABLE IF NOT EXISTS persons (
+	// 					name STRING,
+	// 					age INT)`
+	//
+	// _, err := Db.Exec(cmd)
+
+	// データの追加
+	// cmd := "INSERT INTO persons (name, age) VALUES (?, ?)"
+	//
+	// _, err := Db.Exec(cmd, "Toru", 25)
+
+	// データの更新
+	// cmd := "UPDATE persons SET age = ? WHERE name = ?"
+
+	// _, err := Db.Exec(cmd, 30, "Taro")
+
+	// データの削除
+	cmd := "DELETE FROM persons WHERE name = ?"
+
+	_, err := Db.Exec(cmd, "Toru")
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	// データの取得
+	// cmd := "SELECT * FROM persons WHERE age = ?"
+
+	// QueryRow: 1レコード取得
+	// row := Db.QueryRow(cmd, 25)
+
+	// var p Person
+
+	// err := row.Scan(&p.Name, &p.Age)
+
+	// if err != nil {
+	// 	if err == sql.ErrNoRows {
+	// 		log.Println("No Row")
+	// 	} else {
+	// 		log.Println(err)
+	// 	}
+	// }
+
+	// fmt.Println(p.Name, p.Age)
+}
+
+////////////////////////////////////////
